1b05849de0e32f064339a29a7950f475c878121c
Jordan smith <jordansmith989@gmail.com>
2021-01-11 21:03:13 -0600

    initial type and variable discussion

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nathan";
+
+            int number = Int32.MaxValue;
+
+            Console.WriteLine(number);
+        }
+    }
+}
